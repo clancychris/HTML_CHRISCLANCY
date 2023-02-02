1b1e1b36b6735115bda14e2237dd6d60491c2a50
clancychris <clancychris25@gmail.com>
2023-02-02 13:04:56 +0000

    moved dropdown menu to top right, cleared out some excess code and right-aligned dropdown text

--- script.js
+++ script.js
@@ -6,14 +6,11 @@ function myFunction() {
     x.className = "topnav";
   }
 }
-
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
+5z
 function dropFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Close the dropdown menu if the user clicks outside of it
 window.onclick = function (event) {
   if (!event.target.matches(".dropbtn")) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
